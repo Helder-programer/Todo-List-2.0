b97d0f042a4e8c42bbd7052c6378486bde3f4bcd
Helder Nicollas <nicollashelder@gmail.com>
2023-05-11 01:13:23 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/users';
 import checklistRouter from './routes/checklists';
 import taskRouter from './routes/tasks';
@@ -13,7 +14,7 @@ declare global {
         }
     }
 }
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/users', userRouter);
