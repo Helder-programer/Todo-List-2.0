8b0cc5bc7ba8cb0c48f2693c6dcf9796f38d4f4d
Helder <nicollashelder@gmail.com>
2023-05-12 09:32:44 -0300

    remove invalid bar from server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/users', userRouter);
-app.use('/checklists/', checklistRouter, taskRouter);
+app.use('/checklists', checklistRouter, taskRouter);
 
 app.listen(8000, () => {
     console.log('Servidor Rodando!');
